4588de0952c909c4e1fb76bac8681378b5cb4b90
Cathleen Schmalfuss <cathleen.schmalfuss@de.ibm.com>
2017-07-08 11:09:52 +0200

    made enter work

--- index.js
+++ index.js
@@ -1,18 +1,10 @@
-document.getElementById("input").addEventListener("keydown", function(e) {
-  console.log("test1");
-  if (!e) { var e = window.event; }
-    console.log("test2");
-    e.preventDefault(); // sometimes useful
-    // Enter is pressed
-    console.log("test3");
-    if (e.keyCode == 13) {
-      console.log("test3");
-      submitFunction();
-    }
-}, false);
+window.onload = enter;
 
-submitFunction() {
-  console.log("test3");
-  alert("this worked!");
-  console.log(getElementById("input").value);
+function enter(e) {
+  document.getElementById("input").addEventListener("keydown", function(e) {
+      // submit message on enter
+      if (e.keyCode === 13) {
+        alert("This worked!");
+      }
+    }, false);
 }
